91f3ee9dd0bd1cbb5bf6b18b5440fe66566d6fd8
John Larsen <dmessiahpchan@gmail.com>
2014-02-15 22:54:12 +0100

    Removed gui member from FinishGameOnTrigger

--- Assets/Scripts/FinishGameOnTrigger.cs
+++ Assets/Scripts/FinishGameOnTrigger.cs
@@ -4,20 +4,15 @@ using System.Collections;
 using ModestTree.Zenject;
 
 public class FinishGameOnTrigger : MonoBehaviour
-{
-	public GUITexture endScreenGui;
-	
+{	
 	[Inject]
 	private AudioSource gameMusic;
 	[Inject]
 	private GameController gameController;
 
-
 	// Use this for initialization
 	void OnTriggerEnter(Collider other)
 	{
-		endScreenGui.texture = Resources.Load<Texture>("BrowserHistory/watchKittens");
-		endScreenGui.gameObject.SetActive(true);
 		gameController.isFinished = true;
 	}
 
