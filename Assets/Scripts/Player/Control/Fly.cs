a94cbbf90394819c7026b4af420572ef156b2967
unknown <kiko_trajkovski@hotmail.com>
2014-02-15 10:43:00 +0100

    Controls remade with rigidbody

--- Assets/Scripts/Player/Control/Fly.cs
+++ Assets/Scripts/Player/Control/Fly.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Fly : Controller {
 
 	public float speed;
@@ -13,11 +14,17 @@ public class Fly : Controller {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.Translate(transform.forward * speed);
+//		transform.Translate(transform.forward * speed);
 		float mx = Input.GetAxis("Mouse X");
 		float my = Input.GetAxis("Mouse Y");
 
-		transform.RotateAround(transform.right, -my * Time.deltaTime);
-		transform.RotateAround(transform.up, mx * Time.deltaTime);
+		rigidbody.velocity = transform.forward * speed;
+
+		rigidbody.angularVelocity = transform.TransformDirection( new Vector3(-my, mx, 0f) );
+
+//		transform.RotateAround(transform.right, -my * Time.deltaTime);
+//		transform.RotateAround(transform.up, mx * Time.deltaTime);
+
+
 	}
 }
