0983b3ad0928468781fded59af07712f14aa6ca3
John Larsen <dmessiahpchan@gmail.com>
2014-02-15 11:20:10 +0100

    Pickup object

---
+++ Assets/Scripts/PickupOnContact.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using ModestTree.Zenject;
+
+public class PickupOnContact : MonoBehaviour 
+{
+	[Inject]
+	private GameController gameController;
+
+	void OnTriggerEnter(Collider other)
+	{
+		gameController.collectedPickups.Add(this.gameObject);
+		Destroy(this.gameObject);
+	}
+
+}
