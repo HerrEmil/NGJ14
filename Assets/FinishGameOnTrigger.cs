25d29373910dc24c36c004fc290ed77ce8b77f11
John Larsen <dmessiahpchan@gmail.com>
2014-02-15 13:55:27 +0100

    Delete button
    Land on it to show the fake web pages (replace with a GUITexture)

---
+++ Assets/FinishGameOnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishGameOnTrigger : MonoBehaviour
+{
+	public GUIText BrowserHistoryGui;
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider other)
+	{
+		BrowserHistoryGui.text = Resources.Load<TextAsset>("BrowserHistory").text;
+		BrowserHistoryGui.gameObject.SetActive(true);
+	}
+}
