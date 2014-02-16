9c8d7b76379377d132a4dacb1c0d8e59235e1cca
John Larsen <dmessiahpchan@gmail.com>
2014-02-16 13:49:42 +0100

    Added Intro

---
+++ Assets/Scripts/Finish/ShowIntro.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowIntro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(showIntro());
+	}
+	
+	private IEnumerator showIntro()
+	{
+		
+		while (GameResult.result.isMobile ? Input.touches.Length == 0 : !Input.GetButton("Fire1"))
+		{
+			yield return new WaitForSeconds(0.1f);
+		}
+		
+		Application.LoadLevel("level");
+	}
+}
