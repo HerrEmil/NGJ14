ef606446e349866c9e1255c300d4c99ec17ff7e2
John Larsen <dmessiahpchan@gmail.com>
2014-02-16 09:28:30 +0100

    Updated Win scene

--- Assets/Scripts/Finish/ShowGameStatistics.cs
+++ Assets/Scripts/Finish/ShowGameStatistics.cs
@@ -1,23 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ShowGameStatistics : MonoBehaviour {
 
 	public GUIText collectibles;
+	public Renderer backGroundText;
+	public Texture[] textures;
+
+	public float transitionTime;
 
 	void Start () 
 	{
 		collectibles.text = "Destroyed Evidence: " + GameResult.result.collectedPickups.Count;
-		StartCoroutine (showStatistics(collectibles));
+		StartCoroutine (showSlideShow());
 	}
 
 	private IEnumerator showStatistics(GUIText text)
 	{
+
 		for (var i = 0; i < 10; i++) 
 		{
 			var currentColor = text.color;
 			text.color = new Color(currentColor.r,currentColor.b,currentColor.g, i/10.0f);	
 			yield return new WaitForSeconds(.1f);
+		}
+	}
+
+	private IEnumerator showSlideShow()
+	{
+		foreach (var texture in textures)
+		{
+			backGroundText.material.mainTexture = texture;
+			Debug.Log(texture.name);
+			yield return new WaitForSeconds(transitionTime);
 		}
 	}
 }
