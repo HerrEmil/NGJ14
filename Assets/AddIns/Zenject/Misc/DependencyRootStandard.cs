ceec3d9a647004c3e1470566a7df006a504209bd
Emil Andersson <HerrEmil@users.noreply.github.com>
2026-04-20 11:02:26 +0200

    Suppress Zenject dependency root warnings

--- Assets/AddIns/Zenject/Misc/DependencyRootStandard.cs
+++ Assets/AddIns/Zenject/Misc/DependencyRootStandard.cs
@@ -9,14 +9,26 @@ namespace ModestTree.Zenject
         // but since we define a root so often (eg. unit tests)
         // just use [Inject] for the root classes
 
+        #pragma warning disable 0414
         [Inject]
         IKernel _kernel = null;
+        #pragma warning restore 0414
 
         [Inject]
         EntryPointInitializer _initializer = null;
 
+        protected virtual void AssertDependenciesResolved()
+        {
+            // These injections intentionally force the bootstrapping services to resolve.
+            _ = _kernel;
+            _ = _initializer;
+            Assert.That(_kernel != null);
+            Assert.That(_initializer != null);
+        }
+
         public virtual void Start()
         {
+            AssertDependenciesResolved();
             Debug.Log("Initializing dependency root");
             _initializer.Initialize();
         }
@@ -25,7 +37,16 @@ namespace ModestTree.Zenject
     public class DependencyRootStandard<TRoot> : DependencyRootStandard
         where TRoot : class
     {
+        #pragma warning disable 0414
         [Inject]
         TRoot _root = null;
+        #pragma warning restore 0414
+
+        protected override void AssertDependenciesResolved()
+        {
+            base.AssertDependenciesResolved();
+            _ = _root;
+            Assert.That(_root != null);
+        }
     }
 }
